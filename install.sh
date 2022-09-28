f7ff604b078a5870a5113181d02429e3d3c6751b
Asep Hadad <84879541+lycoxz@users.noreply.github.com>
2022-09-28 18:59:57 +0700

    Update install.sh
    Fixing install

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-#! /usr/bin/bash
-null="> /dev/null 2>&1"
+#!/usr/bin/bash
 g="\033[1;32m"
 r="\033[1;31m"
 b="\033[1;34m"
@@ -7,7 +6,7 @@ w="\033[0m"
 echo ""
 echo -e $b"➤ "$w"Axis For Termux"
 echo ""
-echo -e $b"➤ "$w" prepare for installing dependencies ..."
+echo -e $b"➤ "$w"prepare for installing dependencies ..."
 echo ""
 echo -e $b"➤ "$w" installing pacakge: "$g"php"$w
 pkg install php
@@ -15,11 +14,12 @@ echo ""
 echo -e $b"➤ "$w" installing pacakge: "$g"php curl"$w
 pkg install php curl
 echo ""
-cd $HOME/ClashForTermux
+cd $HOME/AxisForTermux
 sleep 2
 echo -e $b"➤ "$w" coppying apiaxis: "$g"apiaxis"$w
 cp -r -f apiaxis.php $HOME/
 echo ""
 sleep 2
+clear
 cd
 php apiaxis.php
