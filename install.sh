db44b2a087ab5eb2c44096a51fef479afec19bd9
Asep Hadad <84879541+lycoxz@users.noreply.github.com>
2022-09-28 16:53:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,18 +5,24 @@ r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
 
+echo ""
+
 echo -e $b"➤ "$w"Axis For Termux"
+
 echo ""
 
 echo -e $b"➤ "$w" prepare for installing dependencies ..."
+
 echo ""
 
 echo -e $b"➤ "$w" installing pacakge: "$g"php"$w
 pkg install php
+
 echo ""
 
 echo -e $b"➤ "$w" installing pacakge: "$g"php curl"$w
 pkg install php curl
+
 echo ""
 
 cd $HOME/ClashForTermux
@@ -24,6 +30,7 @@ sleep 2
 
 echo -e $b"➤ "$w" coppying apiaxis: "$g"apiaxis"$w
 cp -r -f apiaxis.php $HOME/
+
 echo ""
 
 sleep 2
